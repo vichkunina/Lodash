d56af9608580b0177b172e18bed698134a3bcf18
Daria <vichkunina@bk.ru>
2018-02-02 01:12:08 +0500

    indexOf/intesection/newDifference

--- dashLodash.js
+++ dashLodash.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 exports.chunk = function(array = [], size = 1){
     let resultArray = [];
     while (array.length) {
@@ -15,6 +16,7 @@ exports.compact = function(array = []) {
     for (let i = 0; i < array.length; i++) {
         if (array[i]) resultArray.push(array[i]);
     }
+
     return resultArray;
 }
 
@@ -23,9 +25,9 @@ exports.concat = function() {
     let args = [];
     for (let i = 0; i < arguments.length; i++) {
         args[i] = arguments[i];
-        if (!(args[i] instanceof Array))
+        if (!(args[i] instanceof Array)) {
             resultArray.push(args[i])
-        else {
+        } else {
             for (let j = 0; j < args[i].length; j++) {
                 resultArray.push(args[i][j]);
             }
@@ -35,16 +37,46 @@ exports.concat = function() {
     return resultArray;
 }
 
-exports.difference = function(arrFrom = [], arrWhat) {
-    let resultArray = [];
-    for(let i = 0; i < arrFrom.length; i++)
-        if (!(arrWhat.includes(arrFrom[i]))) {
-            resultArray.push(arrFrom[i]);
-            console.log(arrFrom[i]);
+// exports.difference = function(arrFrom = [], arrWhat) {
+//     let resultArray = [];
+//     for(let i = 0; i < arrFrom.length; i++)
+//         if (!(arrWhat.includes(arrFrom[i]))) {
+//             resultArray.push(arrFrom[i]);
+//             console.log(arrFrom[i]);
+//         }
+//     return resultArray;
+// }
+
+function compareForDifferences(array, currentArray) {
+    var newArray = [];
+    array.forEach(el => {
+        if (!(currentArray.includes(el))) {
+            newArray.push(el);
         }
-    return resultArray;
+    })
+
+    return newArray;
 }
 
+exports.difference = function(ars) {
+
+    if (!arguments.length) {
+        return []
+    } else {
+        var newArr = [];
+        for (var j = 0; j < arguments[0].length; j++) {
+            if (!(arguments[1].includes(arguments[0][j]))) {
+                newArr.push(arguments[0][j]);
+            }
+        }
+
+        for (var k = 2; k < arguments.length; k++) {
+            newArr = compareForDifferences(newArr, arguments[k]);
+        }
+
+        return newArr;
+    }
+}
 
 exports.drop = function(arr = [], size = 1) {
     return arr.slice(size, arr.length);
@@ -69,10 +101,11 @@ exports.head = function(arr) {
 exports.flatten = function(arr = []) {
     let resultArray = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] instanceof Array)
-            for(let j = 0; j < arr[i].length; j++)
+        if (arr[i] instanceof Array) {
+            for(let j = 0; j < arr[i].length; j++) {
                 resultArray.push(arr[i][j]);
-        else resultArray.push(arr[i])
+            }
+        } else resultArray.push(arr[i])
     }
 
     return resultArray;
@@ -81,31 +114,33 @@ exports.flatten = function(arr = []) {
 function getRidOfDepth(arg, temp) {
     if(arg instanceof Array) {
         for (let i = 0; i < arg.length; i++) {
-            if (!(arg[i] instanceof Array))
+            if (!(arg[i] instanceof Array)) {
                 temp.push(arg[i]);
-            else getRidOfDepth(arg[i], temp);
+            } else getRidOfDepth(arg[i], temp);
         };
     } else temp.push(arg);
 }
 
 exports.flattenDeep = function(arr = []) {
     let resultArray = [];
-    getRidOfDepth(arr, resultArray)
+    getRidOfDepth(arr, resultArray);
+
     return resultArray;
 }
 
 exports.flattenDepth = function(arr = [], depth = 1) {
     let resultArray = [];
     getRidOfDefDepth(arr, resultArray, depth);
+
     return resultArray;
 }
 
 function getRidOfDefDepth(arg, temp, depth) {
     for (let i = 0; i < arg.length; i++) {
         if ((arg[i] instanceof Array)) {
-            if (depth)
+            if (depth) {
                 getRidOfDefDepth(arg[i], temp, depth - 1);
-            else temp.push(arg[i]);
+            } else temp.push(arg[i]);
         } else temp.push(arg[i]);
     }
 }
@@ -117,6 +152,56 @@ exports.fromPairs = function(pairs = {}) {
         var onePair = pairs[i];
         result[onePair[0]] = onePair[1];
     }
+
     return result;
 }
 
+exports.indexOf = function(array = [], value, fromIndex = 0) {
+    var result = -1;
+    for(fromIndex; fromIndex < array.length; fromIndex++) {
+        if (array[fromIndex] == value) {
+            result = fromIndex;
+            break;
+        }
+    }
+
+    return result;
+}
+
+exports.initial = function(array) {
+    return exports.dropRight(array, 1);
+}
+
+
+function compareForIntersection(array, currentArray) {
+    var newArray = [];
+    array.forEach(el => {
+        if (currentArray.includes(el)) {
+            newArray.push(el);
+        }
+    })
+
+    return newArray;
+}
+
+exports.intersection = function(ars = []) {
+    var newArr = [];
+    for (var t = 0; t < arguments.length; t++) {
+        if (!(arguments[t] instanceof Array)) {
+            arguments[t] = [];
+        }
+    }
+
+    for (var i = 0; i < arguments[0].length; i++) {
+        for (var j = 0; j < arguments[1].length; j++) {
+            if (arguments[0][i] === arguments[1][j])
+                newArr.push(arguments[0][i]);
+        }
+    }
+
+    for (var k = 2; k < arguments.length; k++) {
+        newArr = compareForIntersection(newArr, arguments[k]);
+    }
+
+    return newArr;
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var test = require('./dashLodash');
 console.log(test.chunk([1, 3, 6, 7, 3, 6, 5], 2));
 console.log(test.concat([1, 6, 8], 2, [3], [[4]]));
 console.log(test.compact([0, 1, false, 2, '', NaN, null, 'rt', undefined]));
-console.log(test.difference([1, 3, 1], [1, 6, 7]));
+console.log(test.difference([2, 1], [1], []));
 console.log(test.drop([1, 2, 3], 2));
 console.log(test.dropRight([1, 2, 3]));
 console.log(test.fill([4, 6, 8, 10], '*', 1, 3));
@@ -11,3 +11,6 @@ console.log(test.flatten([1, [2, [3, [4]], 5]]))
 console.log(test.flattenDeep([1, [2, [3, [4]], 5]]));
 console.log(test.flattenDepth([1, [2, [3, [4]], 5]], 2));
 console.log(test.fromPairs([['a', 1], ['b', 2]]));
+console.log(test.indexOf([1, 2, 7, 1, 2, 7, 7], 7, 3));
+console.log(test.initial([1, 2, 3, 5]));
+console.log(test.intersection([2, 3], [1, 3], [ 3, 6], [3, 3]))
